70c78ee610177f16348eaa835b6c39d0806e9d48
CharlieIsSmile <96523051+CharlieIsSmile@users.noreply.github.com>
2024-05-29 14:47:13 +0800

    Delete matrix-connect directory

--- matrix-connect/src/main/java/cn/qfei/connect/common/SystemConstant.java
+++
@@ -1,9 +0,0 @@
-package cn.qfei.connect.common;
-
-public class SystemConstant {
-    public static final Boolean isDeletedNo = false;
-    public static final Boolean isDeletedYes = true;
-
-    public static final Integer UserNo=1;
-    public static final Integer UserYes=0;
-}
